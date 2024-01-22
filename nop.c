8c5d7df6fc811e870cfe9d6585a15c7e83df6563
samuelsolo <samuelsolomon.des@gmail.com>
2024-01-21 18:35:15 -0800

    Monty

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop -  Does nothing
+ * @stack: does nothing
+ * @line_cnt : does nothing
+ *
+ * Return: NOTHING
+ */
+void nop(stack_t **stack, unsigned int line_cnt)
+{
+	(void) stack;
+	(void) line_cnt;
+}
